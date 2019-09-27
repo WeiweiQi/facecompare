2e3b6e32c94cec9976136dde06dc03d86e8d3d50
WeiweiQi <l>
2019-09-27 17:35:55 +0800

    change html
    change html

--- src/main/java/com/qiweiwei/facecompare/FaceCompareController.java
+++ src/main/java/com/qiweiwei/facecompare/FaceCompareController.java
@@ -1,7 +1,11 @@
 package com.qiweiwei.facecompare;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.upload.UploadFile;
 
 public class FaceCompareController extends Controller{
 	
@@ -14,6 +18,12 @@ public class FaceCompareController extends Controller{
 	
 	public void previewcut() {
 		render("preview/index.html");
+	}
+	
+	public void uploadimage() {
+		//UploadFile file = getFile();
+		String base64 = getPara("base64code");
+		renderJson(new Record().set("success", "1").set("code", "000001"));
 	}
 
 }
