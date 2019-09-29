d76e0c233bfdf0f4e3d442f656b6fa0a8f7089f4
WeiweiQi <l>
2019-09-29 17:05:02 +0800

    change the image size css
    change the image size css

--- src/main/java/com/qiweiwei/facecompare/FaceCompareController.java
+++ src/main/java/com/qiweiwei/facecompare/FaceCompareController.java
@@ -1,11 +1,8 @@
 package com.qiweiwei.facecompare;
 
-import javax.servlet.http.HttpServletRequest;
-
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Record;
-import com.jfinal.upload.UploadFile;
 
 public class FaceCompareController extends Controller{
 	
@@ -23,7 +20,8 @@ public class FaceCompareController extends Controller{
 	public void uploadimage() {
 		//UploadFile file = getFile();
 		String base64 = getPara("base64code");
-		renderJson(new Record().set("success", "1").set("code", "000001"));
+		base64 = base64.replaceAll(" ", "+");
+		renderJson(new Record().set("success", "1").set("code", "000001").set("base64", base64));
 	}
 
 }
